ce140087dc8011ba658c84f45c1aaef14807afc9
sachithrakalani <kalanisathyani@gmail.com>
2024-09-25 19:22:38 +0530

    Create Server for tode app

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+const express = require ('express');
+const app = express();
+
+//Create Server
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
+
+//Create Responce
+app.get("/",(req,res)=>[
+    res.send('Hello Node')
+]);
