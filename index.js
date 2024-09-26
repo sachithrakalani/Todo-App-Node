3ead4bfa3746f8be14c261f273b0091f5c934811
sachithrakalani <kalanisathyani@gmail.com>
2024-09-26 09:34:41 +0530

    Create Database using mongodb

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const express = require ('express');
 const mongoose = require("mongoose");
 const app = express();
 
-//Create Server
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+
 
 //Create Responce
 app.get("/",(req,res)=>[
@@ -16,6 +13,10 @@ app.get("/",(req,res)=>[
 mongoose.connect("mongodb+srv://sathyangitodoapp:ZiH6We8VeQGHG8k6@todo-app.iglmv.mongodb.net/Todo-App?retryWrites=true&w=majority&appName=Todo-App")
     .then(() => {
         console.log("Connected to Database!");
+        //Create Server
+        app.listen(3000,()=>{
+            console.log("Server is running on port 3000");
+        })
     })
     .catch((error)=>{
         console.log(error)
